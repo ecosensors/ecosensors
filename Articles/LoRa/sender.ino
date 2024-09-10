72b48d998e1533a90bd59cd1ce7d091a723a565c
EcoSensors <info@eco-sensors.ch>
2024-09-11 00:41:33 +0200

    Update sender.ino

--- Articles/LoRa/sender.ino
+++ Articles/LoRa/sender.ino
@@ -1,6 +1,5 @@
 /*
-* That script has been written and tested in december 2017.
-* The script has not been reviewed since.
+* That script has been written and tested in december 2017. The script has not been reviewed since.
 */
 
 #include <Keyboard.h>               // Need only to get the standard values of keyboard keys.
